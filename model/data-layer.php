4081d1c28bf675ef9631f29b26dc8416fcd31e98
Austin Parker <aparker24@mail.greenriver.edu>
2020-06-24 03:11:42 -0400

    Added interests page

---
+++ model/data-layer.php
@@ -0,0 +1,12 @@
+<?php
+
+function getInterests1()
+{
+    return array("tv", "movies", "cooking", "board games", "puzzles", "reading", "playing cards", "video games");
+}
+
+
+function getInterests2()
+{
+    return array("hiking", "biking", "swimming", "collecting", "walking", "climbing");
+}
